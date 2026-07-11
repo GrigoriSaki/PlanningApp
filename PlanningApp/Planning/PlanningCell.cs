65ad87ac892eaaee615281775813ba60027b4331
Grzegorz Wisniowski <grzesiekwis200@gmail.com>
2026-07-11 10:08:43 +0200

    ScheduleAssignment MVP complete

---
+++ PlanningApp/Planning/PlanningCell.cs
@@ -0,0 +1,13 @@
+﻿namespace PlanningApp.Planning
+{
+    public class PlanningCell
+    {
+        public DateTime WorkDate { get; set; }
+
+        public int Shift { get; set; }
+
+        public int ProductionLineId { get; set; }
+
+        public string Position { get; set; } = "";
+    }
+}
