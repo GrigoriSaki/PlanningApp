88b348587ff5e1c85ce1964873ddb8b0a3ce2d18
Grzegorz Wisniowski <grzesiekwis200@gmail.com>
2026-07-13 20:38:32 +0200

    changing week number feature complete

--- PlanningApp/obj/Debug/net10.0/PlanningApp.AssemblyInfo.cs
+++ PlanningApp/obj/Debug/net10.0/PlanningApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlanningApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65ad87ac892eaaee615281775813ba60027b4331")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c0449a9e7d360ab5e2d25fde14e1222079fcd29")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlanningApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlanningApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
